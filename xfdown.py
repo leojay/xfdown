e0ec1315833add9194f4fa9e9e4591b15d635ba7
Leo Jay <python.leojay@gmail.com>
2013-01-25 18:06:26 +0800

    add method "list_tasks", it returns a list of "TaskInfo"

--- xfdown.py
+++ xfdown.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 
-import subprocess
 import random
 import json, os, sys, re, hashlib
 
@@ -59,6 +58,18 @@ def get_gtk(strs):
     return hash & 0x7fffffff
 
 
+class TaskInfo(object):
+    def __init__(self, task_id, hash, filename, file_size, completed_size, status):
+        self.id = task_id
+        self.hash = hash
+        self.filename = filename
+        self.file_size = file_size
+        self.completed_size = completed_size
+        self.status = status
+
+    def is_completed(self):
+        return self.file_size > 0 and self.file_size == self.completed_size
+
 class XF:
     """
      Login QQ
@@ -109,10 +120,8 @@ class XF:
         opener.addheaders = [('User-Agent', 'Mozilla/5.0'), ("Referer", "http://lixian.qq.com/main.html")]
         request.install_opener(opener)
 
-        if cookieload:
-            self.main()
-        else:
-            self.__Login(True)
+        if not cookieload:
+            self.__Login()
 
     def __request(self, url, data=None):
         """
@@ -144,38 +153,13 @@ class XF:
             imgurl = "http://captcha.qq.com/getimage?aid=567008010&r=%s&uin=%s" % (random.Random().random(), self.__qq)
             with open(self.__verifyimg, "wb") as f:
                 f.write(request.urlopen(imgurl).read())
-            try:
-                subprocess.Popen(['xdg-open', self.__verifyimg])
-            except:
-                _print("请打开%s查看验证码" % self.__verifyimg)
-            print("请输入验证码：")
             vf = raw_input("vf # ").strip()
             verify[1] = vf
 
         return verify
 
 
-    def __request_login(self):
-        urlv = "http://ptlogin2.qq.com/login?u=%s&p=%s&verifycode=%s" % (self.__qq, self.passwd, self.__verifycode[1]) + "&aid=567008010&u1=http%3A%2F%2Flixian.qq.com%2Fmain.html&h=1&ptredirect=1&ptlang=2052&from_ui=1&dumy=&fp=loginerroralert&action=2-10-&mibao_css=&t=1&g=1"
-        str = self.__request(urlv)
-        if str.find(_('登录成功')) != -1:
-            self.__getlogin()
-            self.main()
-        elif str.find(_('验证码不正确')) != -1:
-            self.__getverifycode()
-            self.__Login(False, True)
-        elif str.find(_('不正确')) != -1:
-            _print('你输入的帐号或者密码不正确，请重新输入。')
-            self.__Login(True)
-        else:
-            #print('登录失败')
-            _print(str)
-            self.__Login(True)
-
-    def main(self):
-        self.__getlist()
-
-    def getfilename_url(self, url):
+    def __get_default_filename(self, url):
         url = url.strip()
         filename = ""
         if url.startswith("ed2k"):
@@ -191,92 +175,81 @@ class XF:
         skey = re.findall('skey="([^"]+)"', open(self.__cookiepath).read())[0]
         return self.__request(LOGIN_URL, {"g_tk": get_gtk(skey)})
 
-    def __getlist(self):
+    def list_tasks(self):
         """
-        得到任务名与hash值
+        @rtype: list of TaskInfo
         """
         res = self.__request(LIST_URL)
         res = json.JSONDecoder().decode(res)
         if res["msg"] == _('未登录!'):
             res = json.JSONDecoder().decode(self.__getlogin())
             if res["msg"] == _('未登录!'):
-                self.__Login()
+                if not self.__Login():
+                    return None
+            return self.list_tasks()
 
-            else:
-                self.main()
-        elif not res["data"]:
-            print(_('无离线任务!'))
-            self.main()
-        else:
-            self.filename = []
-            self.filehash = []
-            self.filemid = []
-            res['data'].sort(key=lambda x: x["file_name"])
-            _print("\n===================离线任务列表====================")
-            _print("序号\t大小\t进度\t文件名")
-            for num in range(len(res['data'])):
-                index = res['data'][num]
-                self.filename.append(index['file_name'].encode("u8"))
-                self.filehash.append(index['code'])
-                size = index['file_size']
-                self.filemid.append(index['mid'])
-                if size == 0:
-                    percent = "-0"
-                else:
-                    percent = str(index['comp_size'] / size * 100).split(".")[0]
+        if not res["data"]:
+            return []
 
-                dw = ["B", "K", "M", "G"]
-                for i in range(4):
-                    _dw = dw[i]
-                    if size >= 1024:
-                        size = size / 1024
-                    else:
-                        break
-                size = "%.1f%s" % (size, _dw)
-                out = "%d\t%s\t%s%%\t%s" % (num + 1, size, percent, _(self.filename[num]))
-                if num % 2 == 0 and os.name == 'posix':
-                    out = "\033[m\033[40m%s\033[m" % out
+        res['data'].sort(key=lambda x: x["file_name"])
+        result = []
+        for entry in res['data']:
+            size = int(entry['file_size'])
+            completed = int(entry['comp_size'])
+            status = int(entry['dl_status'])
+            filename = entry['file_name'].encode("u8")
+            result.append(TaskInfo(entry['mid'], entry['code'], filename, size, completed, status))
+        return result
 
-                _print(out)
-            _print("=======================END=========================\n")
-
-    def get_download_info(self, filename, filehash):
-        data = {'hash': filehash, 'filename': filename, 'browser': 'other'}
+    def get_download_info(self, task):
+        """
+        @type task: TaskInfo
+        """
+        data = {'hash': task.hash, 'filename': task.filename, 'browser': 'other'}
         result = self.__request(DOWNLOAD_INFO_URL, data)
         url = re.search(r'\"com_url\":\"(.+?)\"\,\"', result).group(1)
         cookie = re.search(r'\"com_cookie":\"(.+?)\"\,\"', result).group(1)
         return url, cookie
 
-    def get_aria2c_cmd_line(self, filename, filehash):
-        url, cookie = self.get_download_info(filename, filehash)
+    def get_aria2c_cmd_line(self, task):
+        """
+        @type task: TaskInfo
+        """
+        url, cookie = self.get_download_info(task)
         return "aria2c -c -s10 -x10 --header 'Cookie:ptisp=edu; FTN5K=%s' '%s'" % (cookie, url)
 
-    def delete_task(self, task_id):
-        return self.__request(DELETE_URL, {'mids': task_id})
+    def delete_task(self, task):
+        """
+        @type task: TaskInfo
+        """
+        return self.__request(DELETE_URL, {'mids': task.id})
 
     def add_task(self, url):
-        filename = self.getfilename_url(url)
+        filename = self.__get_default_filename(url)
         data = {"down_link": url,
                 "filename": filename,
                 "filesize": 0,
                 }
         return self.__request(ADD_URL, data)
 
-    def __Login(self, needinput=False, verify=False):
-        """
-        登录
-        """
-        self.__verifycode = self.__getverifycode()
-        self.passwd = self.__preprocess(
-            verifycode=self.__verifycode,
-            hashpasswd=self.hashpasswd
-        )
-        self.__request_login()
+    def __Login(self):
+        verifycode = self.__getverifycode()
+        passwd = self.__preprocess(verifycode=verifycode, hashpasswd=self.hashpasswd)
+        urlv = "http://ptlogin2.qq.com/login?u=%s&p=%s&verifycode=%s" % (self.__qq, passwd, verifycode[1]) + "&aid=567008010&u1=http%3A%2F%2Flixian.qq.com%2Fmain.html&h=1&ptredirect=1&ptlang=2052&from_ui=1&dumy=&fp=loginerroralert&action=2-10-&mibao_css=&t=1&g=1"
+        if _('登录成功') in self.__request(urlv):
+            self.__getlogin()
+            return True
+        return False
 
 def main():
     f = open('credential', 'rb')
     xf = XF(f.next().strip(), f.next().strip())
     xf.start()
+    for task in xf.list_tasks():
+        print task.filename, task.completed_size, task.file_size
+        if task.is_completed():
+            print xf.get_aria2c_cmd_line(task)
+            print
 
 if __name__ == '__main__':
     main()
